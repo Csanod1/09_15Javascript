03f390a8025f823e8248a3f59d467fe560e13bbc
csanad kormos <csanadsuli19@gmail.com>
2022-09-15 13:00:57 +0200

    két függvény exportálása

--- src/lista.js
+++ src/lista.js
@@ -1,1 +1,17 @@
 let lista = [];
+
+export function hozzaad(szam){
+    if(szam < 0){
+        console.log("HIBAA! adjon meg egy -1-nél nagyobb számot");
+    }else{
+        lista.push(szam);
+    }
+}
+export function atlag(bemenoLista){
+    let szamlalo = 0;
+    for(let i = 0; i < bemenoLista.length; i++){
+        szamlalo += lista[i];
+
+    }
+    return szamlalo/lista.length;
+}
